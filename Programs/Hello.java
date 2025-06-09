4d06a4620195085e94caab3456b819700e94c42e
rajashreey841 <rajashreey841@gmail.com>
2025-06-09 23:15:56 +0000

    Practicing

---
+++ Programs/Hello.java
@@ -0,0 +1,10 @@
+class Hello
+{
+    public static void main (String[] args)
+    {
+        int x = 5;
+        float y = 3.14f;
+        float result= ++y;
+        System.out.println(result) ;
+           }
+}
