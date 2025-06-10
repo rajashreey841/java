97757e2192e68e05d83e51a864442ab36c1666b8
rajashreey841 <rajashreey841@gmail.com>
2025-06-10 16:09:51 +0000

    Practicing

--- Programs/Hello.java
+++ Programs/Hello.java
@@ -2,18 +2,17 @@ class Hello
 {
     public static void main (String[] args)
     {
-        int  i =1;
-        while(i<=4)
+        int  i =5;
+        // Using do-while loop condition
+        // will be checked after executing the loop body    
+          
+
+        do
         {
             System.out.println("Hello World " + i);
-            int j =1;
-            while(j<=3)
-            {
-                System.out.println("Hello" + i + " " + j);
-                j++;
-            }
+            
             i++;
-        }
-       
+        }while(i<=4);
+;       
     }
 }
