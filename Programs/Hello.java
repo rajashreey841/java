9e4d3af4b953f5bff5820d4ffb60165c2d0e3274
rajashreey841 <rajashreey841@gmail.com>
2025-06-10 16:07:08 +0000

    Practicing

--- Programs/Hello.java
+++ Programs/Hello.java
@@ -2,18 +2,18 @@ class Hello
 {
     public static void main (String[] args)
     {
-        int x = 5;
-        int result = 0;
-        // Using a ternary operator to determine the value of result based on the value of x
-        result = (x % 2 == 0) ? 10 : 20;
-        // Using ternary operator to assign value to result
-        // The above code is equivalent to the following if-else statement
-        // if (x % 2 == 0) {
-        //     result = 10;
-        // } else {
-        //     result = 20;
-        // }
-        // Print the result 
-        System.out.println("Result is: " + result);
+        int  i =1;
+        while(i<=4)
+        {
+            System.out.println("Hello World " + i);
+            int j =1;
+            while(j<=3)
+            {
+                System.out.println("Hello" + i + " " + j);
+                j++;
+            }
+            i++;
+        }
+       
     }
 }
