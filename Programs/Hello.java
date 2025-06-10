f644a59dbe9a42976e7c9ee334ff303d78fcab8d
rajashreey841 <rajashreey841@gmail.com>
2025-06-10 15:33:41 +0000

    Program

--- Programs/Hello.java
+++ Programs/Hello.java
@@ -4,11 +4,13 @@ class Hello
     {
         int x = 5;
         int y = 10;
+        int  z = 15;
         if (x>y)
-        
             System.out.println(x);
         
-        else
+        else if (y>z)
             System.out.println(y) ;
+        else
+            System.out.println(z);
     }
 }
