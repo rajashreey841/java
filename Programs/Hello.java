4cac683aaa822465d9dc87f0db57282aae544347
rajashreey841 <rajashreey841@gmail.com>
2025-06-10 15:43:51 +0000

    Pragram

--- Programs/Hello.java
+++ Programs/Hello.java
@@ -3,14 +3,17 @@ class Hello
     public static void main (String[] args)
     {
         int x = 5;
-        int y = 10;
-        int  z = 15;
-        if (x>y)
-            System.out.println(x);
-        
-        else if (y>z)
-            System.out.println(y) ;
-        else
-            System.out.println(z);
+        int result = 0;
+        // Using a ternary operator to determine the value of result based on the value of x
+        result = (x % 2 == 0) ? 10 : 20;
+        // Using ternary operator to assign value to result
+        // The above code is equivalent to the following if-else statement
+        // if (x % 2 == 0) {
+        //     result = 10;
+        // } else {
+        //     result = 20;
+        // }
+        // Print the result 
+        System.out.println("Result is: " + result);
     }
 }
